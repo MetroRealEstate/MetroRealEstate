36ba83aaa0025cfa463a0a8a7a061f88a947ce1b
Martín Matías <martin_matias@hotmail.com.ar>
2023-07-14 19:07:05 -0300

    Updated San Bernardio Project Name; Updated San Bernardino Parcel No.

--- Scripts/chnge.py
+++ Scripts/chnge.py
@@ -6,28 +6,27 @@ from tkinter.ttk import Progressbar
 import time
 import os
 import pdfplumber
-from excluded_phrases import excluded_phrases
-from saveintemplate import save_in_template
-from db import save_data_to_mongodb
+from functions.excluded_phrases import excluded_phrases
+from functions.saveintemplate import save_in_template
+from functions.db import save_data_to_mongodb
 
-# Variables globales
+# Global variables
 pdf_path = ''
 pdf_name = ''
 start_time = 0
 
-# Función para buscar la primera fecha y el nombre del proyecto en un archivo PDF y guardarlos en Excel
+# Function to search for the first date and project name in a PDF file and save them in Excel
 
-
-def buscar_datos_en_pdf():
+def search_data():
     global pdf_path, pdf_name, start_time
 
-    # Abrir el archivo PDF
+    # Open the PDF file
     with pdfplumber.open(pdf_path) as pdf:
-        # Crear un libro de Excel y seleccionar la primera hoja
+        # Create an Excel workbook and select the first sheet
         workbook = Workbook()
         sheet = workbook.active
 
-        # Escribir encabezados en Excel
+        # Write headers in Excel
         sheet['A1'] = 'Meeting type'
         sheet['B1'] = 'Meeting Date'
         sheet['C1'] = 'Project Name'
@@ -40,9 +39,9 @@ def buscar_datos_en_pdf():
         sheet['J1'] = 'Existing Used'
         sheet['K1'] = 'Propose Zoning'
         sheet['L1'] = 'Current Application Status'
-        sheet['M1'] = 'comments'
+        sheet['M1'] = 'Comments'
 
-        # Variables para almacenar los resultados
+        # Variables to store the results
         meeting_type = ''
         meeting_date = ''
         project_names = []
@@ -56,12 +55,13 @@ def buscar_datos_en_pdf():
 
         meeting_type = ''
 
-        # Después de extraer el texto del PDF
+        # After extracting the text from the PDF
         text = ""
         for page in pdf.pages:
             text += page.extract_text()
 
-        # Verificar si la frase "PLANNING COMMISSION" está presente en cualquier variante
+        # Check if the phrase "PLANNING COMMISSION" is present in any variant
+
         meeting_regex = r'PLANNING COMMISSION|Planning and Housing Commission'
         meeting_match = re.search(meeting_regex, text, re.I | re.M)
         if meeting_match: meeting_type = "Planning Commission Regular Meeting"
@@ -69,31 +69,56 @@ def buscar_datos_en_pdf():
 
         date_regex = r'\b\d{4}-\d{2}-\d{2}\b|\b\d{1,2}/\d{1,2}/\d{2,4}\b|\b(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)[a-z]* \d{1,2}(?:st|nd|rd|th)?,? \d{2,4}\b'
 
-        # Buscar todas las fechas en el PDF
+        # Find all dates in the PDF
+
         all_dates = re.findall(date_regex, text)
 
-        # Obtener la primera fecha encontrada en el documento
+        # Get the first date found in the document
+
         meeting_date = all_dates[0] if all_dates else '-'
 
-        # Buscar los nombres de proyecto que coincidan con el regex según el contenido del documento
+        # Search for project names that match the regex based on the document content
+
         if "Moreno Valley" in text:
             project_name_regex = r'PEN\d{2}-\d{4}'
         elif "City of Corona" in text or "Corona City" in text:
             project_name_regex = r'(?:PPM\d{4}-\d{4}|PM\s\d+|PP\d{4}-\d{4})|GPA\d{4}-\d{4}|CUP\d{4}-\d{4}'
         elif "City of Lake Elsinore" in text:
             project_name_regex = r'\d{4}-\d{2}'
+        elif "City of Hemet" in text:
+            project_name_regex = r'\d{1,2}-\d{3}'
+        elif "Santa Fe Spring" in text:
+            project_name_regex = r'\d{5}'
         else:
-            project_name_regex = r''  # Define un regex vacío si no se cumple ninguna condición
+            project_name_regex = r''
 
         project_name_matches = re.findall(project_name_regex, text)
         project_names = list(set(project_name_matches)) if project_name_matches else ['-']
 
-        # Buscar la información de parcela en el formato "{###-###-###}"
+        # Search for parcel information in the format "{###-###-###}"
+
         parcel_number_regex = r"\d\d\d-\d\d\d-\s?\d\d\d"
         parcel_number_matches = re.findall(parcel_number_regex, text)
-        parcel_numbers = list(set(parcel_number_matches)) if parcel_number_matches else []
 
-        # Buscar todas las ubicaciones que coincidan con coordenadas o cualquier nombre en cualquier formato que represente un lugar físico
+        if not parcel_number_matches:
+            pattern = r"Tract(?:\s+Map)?\s+(\d+)"
+            match = re.search(pattern, text, re.I)
+            if match:
+                tract_number = match.group(1)
+                parcel_numbers = ['Tract No. ' + tract_number]
+            else:
+                # Use \d{4}-\d{3}-\d{3} pattern
+                parcel_number_regex = r"APN:([\s\S]*?)\d{4}-\d{3}-?\s?\d{3}"
+                parcel_number_matches = re.findall(parcel_number_regex, text, re.M)
+                if parcel_number_matches:
+                    parcel_numbers = list(set(parcel_number_matches))
+                else:
+                    parcel_numbers = []
+        else:
+            parcel_numbers = list(set(parcel_number_matches))
+
+        # Search for all locations that match coordinates or any name in any format representing a physical place
+
         location_regex = r'Location:\s*(.*?)\s*[\n\r]'
         location_matches = re.findall(location_regex, text, re.I | re.M)
         site_regex = r'Project Site:\s*(.*?)\s*[\n\r]'
@@ -105,43 +130,42 @@ def buscar_datos_en_pdf():
         locations = []
         for location in location_matches:
             if re.search(r'(\d+(\.\d+)?),\s*(\d+(\.\d+)?)', location):
-                # Si la ubicación coincide con coordenadas, se agrega a la lista de ubicaciones
+                # If the location matches coordinates, add it to the locations list
                 locations.append(location)
             else:
-                # Si la ubicación no coincide con coordenadas, se verifica si contiene algún nombre que represente un lugar físico
+                # If the location doesn't match coordinates, check if it contains any name representing a physical place
                 if re.search(r'\b[A-Za-z\s]+\b', location):
                     locations.append(location)
 
         for location in site_matches:
             if re.search(r'(\d+(\.\d+)?),\s*(\d+(\.\d+)?)', location):
-                # Si la ubicación coincide con coordenadas, se agrega a la lista de ubicaciones
+                # If the location matches coordinates, add it to the locations list
                 locations.append(location)
             else:
-                # Si la ubicación no coincide con coordenadas, se verifica si contiene algún nombre que represente un lugar físico
+                # If the location doesn't match coordinates, check if it contains any name representing a physical place
                 if re.search(r'\b[A-Za-z\s]+\b', location):
                     locations.append(location)
 
         for location in located_matches:
             if re.search(r'(\d+(\.\d+)?),\s*(\d+(\.\d+)?)', location):
-                # Si la ubicación coincide con coordenadas, se agrega a la lista de ubicaciones
+                # If the location matches coordinates, add it to the locations list
                 locations.append(location)
             else:
-                # Si la ubicación no coincide con coordenadas, se verifica si contiene algún nombre que represente un lugar físico
+                # If the location doesn't match coordinates, check if it contains any name representing a physical place
                 if re.search(r'\b[A-Za-z\s]+\b', location):
                     locations.append(location)
 
         for location in located_on_matches:
             if re.search(r'(\d+(\.\d+)?),\s*(\d+(\.\d+)?)', location):
-                # Si la ubicación coincide con coordenadas, se agrega a la lista de ubicaciones
                 locations.append(location)
             else:
-                # Si la ubicación no coincide con coordenadas, se verifica si contiene algún nombre que represente un lugar físico
                 if re.search(r'\b[A-Za-z\s]+\b', location):
                     locations.append(location)
                     
         project_locations = list(set(locations)) if locations else ['-']
 
-        # Buscar el estado de la aplicación ("Approved" o "Approval") en cualquier formato o coincidencia
+        # Search for the application status ("Approved" or "Approval") in any format or variation
+
         application_status_regex = r'\b(APPROVED|APPROVAL)\b'
         application_status_match = re.search(application_status_regex, text)
 
@@ -150,51 +174,64 @@ def buscar_datos_en_pdf():
         else:
             application_status = '-'
 
-        # Buscar los nombres de solicitantes
+       # Search for applicant names
+        applicant_regex = r"Applicant: ([A-Z][a-z]+ [A-Z][a-z]+)"
+        applicant_matches = re.findall(applicant_regex, text, re.I | re.M)
+        if not applicant_matches:
+            applicant_regex = r"\b[A-Z][a-z]+ [A-Z][a-z]+\b(?:\sDevelopment Group)?"
+            applicant_matches = re.findall(applicant_regex, text)
+        unique_applicants = list(set(applicant_matches))
+        filtered_applicants = [applicant for applicant in unique_applicants if applicant not in excluded_phrases]
+        applicants = filtered_applicants or ['-']
 
-        applicant_regex = r'\b[A-Z][a-z]+ [A-Z][a-z]+\b(?:\sDevelopment Group)?'
-        applicant_matches = re.findall(applicant_regex, text)
-        applicants = list({match for match in applicant_matches if match not in excluded_phrases}) or ['-']
+        # Search for building size with the format if not found in the previous format
 
-        # Buscar el tamaño del edificio con el formato si no se encuentra en el formato anterior
         building_size_regex = r'\(?(\d{1,3}(?:,\d{3})*(?:\.\d+)?)\)?\s*(SQUARES?\s*FEETS?|sq.\s*ft|SF|SQUARES?\s*FooTS?)'
         building_size_match = re.findall(building_size_regex, text, re.I)
-        building_sizes = [
-            size[0].strip('()') + ' SF' for size in building_size_match] if building_size_match else ['-']
+        building_sizes = list(set([size[0].strip('()') + ' SF' for size in building_size_match])) if building_size_match else ['-']
 
-        # Buscar los tamaños de tierra en acres en los formatos "2.6 acre", "33.57-acre" o "18.49- acre site"
+        # Search for land sizes in acres with the formats "2.6 acre", "33.57-acre", or "18.49- acre site"
+
         land_size_regex = r'(\d+(?:\.\d+)?)\s*(?:-)?\s*acre\s*(?:site)?'
         land_size_matches = re.findall(land_size_regex, text, re.I)
-        land_sizes = [size + ' acre' for size in land_size_matches] if land_size_matches else ['-']
+        land_sizes = list(set([size + ' acre' for size in land_size_matches])) if land_size_matches else ['-']
 
-        # Buscar las propuestas después de "Proposal" y guardar el texto hasta el primer punto
+        # Search for proposals after "Proposal" and save the text until the first period
+
         moreno_proposal_regex = r'(?:Proposal|Proposed Project|Proposal: |Proposed Project: )\b([^.:]*\d+(?:\.\d+)?[^.]*)\.'
         corona_proposal_regex = r'PUBLIC HEARING\s*-\s*([^*]+?)\bApplicant:'
         elsinore_proposal_regex = r'\bID#\s\d{2}-\d{3}\b\s*((?:(?!(?:Attachments\b|.*\bcoronavirus\b)).)*)'
+        hemet_proposal_regex = r'PROJECT SUMMARY:\s*(.*?\.)'
 
-        proposal_matches = re.findall(moreno_proposal_regex, text, re.S)
-        if not proposal_matches:
-            proposal_matches = re.findall(corona_proposal_regex, text, re.S)
-        if not proposal_matches:
-            proposal_matches = [match.group(1).strip() for match in re.finditer(elsinore_proposal_regex, text, re.S | re.IGNORECASE) if 'coronavirus' not in match.group(1).lower()]
+        proposals = re.findall(moreno_proposal_regex, text, re.S)
+        if not proposals:
+            proposals = re.findall(corona_proposal_regex, text, re.S)
+        if not proposals:
+            proposals = [match.group(1).strip() for match in re.finditer(elsinore_proposal_regex, text, re.S | re.IGNORECASE) if 'coronavirus' not in match.group(1).lower()]
+        if not proposals:
+            hemet_matches = re.findall(hemet_proposal_regex, text, re.S | re.M)
+            proposals = [match.strip() for match in hemet_matches]
 
-        captura = proposal_matches[0] if proposal_matches else '-'
-        proposals = proposal_matches if proposal_matches else ['-']
+        captura = proposals[0] if proposals else '-'
 
-        # Buscar la disponibilidad del terreno
+        # Search for existing use
 
         existing_used = []
-        # Buscar la palabra "existing" seguida de la frase completa hasta el primer punto
+
+        # Search for the word "existing" followed by the complete phrase until the first period
+
         existing_regex = r'(?i)\bexisting\b[^.]*'
         existing_matches = re.findall(existing_regex, text)
         existing_used = existing_matches if existing_matches else ['-']
 
-        # Buscar las propuestas de zonificación
+        # Search for propose zoning
+
         propose_zoning_regex = r'(?i)\b(?:proposal|PUBLIC HEARING)\b.*?\b(Construction|subdivide|Expansion|Merge|Remodel|Subdivition(?:\s+and)?(?:\s+Construction)?|Development|Demolition(?:\s+and\s+construction)?)\b'
         propose_zoning_matches = re.findall(propose_zoning_regex, text, re.I | re.S | re.M)
         propose_zoning = propose_zoning_matches if propose_zoning_matches else '-'
 
-        # Escribir los resultados en Excel
+        # Write the results to Excel
+
         sheet['A2'] = meeting_type
         sheet['B2'] = meeting_date
         sheet['C2'] = '; '.join(project_names)
@@ -208,13 +245,14 @@ def buscar_datos_en_pdf():
         sheet['K2'] = '; '.join(propose_zoning)
         sheet['L2'] = application_status
 
-        # Guardar el archivo de Excel
+        # Save the Excel file
+
         excel_path = f'{pdf_name}_results.xlsx'
         workbook.save(excel_path)
 
-        # Guardar los datos en la primera fila libre del archivo "COPIA PLANTILLA"
+        # Save the data in the first empty row of the "COPIA PLANTILLA" file
 
-        # Crear una lista con los datos recolectados
+        # Create a list with the collected data
 
         data = [
             meeting_type,
@@ -231,11 +269,11 @@ def buscar_datos_en_pdf():
             application_status,
         ]
 
-        # Llamada a la función para guardar en la plantilla de excel los datos
-
+        #Call the function to save the data in the excel template
+        
         save_in_template(data, 'COPIA PLANTILLA.xlsx')
 
-        # Llamada a la función para guardar los datos en MongoBD
+        # Call the function to save the data in MongoDB
 
         save_data_to_mongodb({
             'meeting_type': meeting_type,
@@ -252,14 +290,14 @@ def buscar_datos_en_pdf():
             'application_status': application_status
         })
 
-        # Mostrar mensaje de finalización y tiempo total de ejecución
-        lbl_mensaje.config(
-            text=f'La búsqueda ha finalizado. Tiempo total: {time.time() - start_time:.2f} segundos')
+        # Display completion message and total execution time
+        lbl_messagge.config(
+            text=f'The search has been finished. Total time: {time.time() - start_time:.2f} segundos')
 
-        # Abrir el archivo Excel después de guardarlo
+        # Open the Excel file after saving
         os.startfile(excel_path)
 
-        # Mostrar resultados en consola
+       # Display results in the console
         print(f"Meeting Type: {meeting_type}")
         print(f"Meeting Date: {meeting_date}")
         print(f"Project Names: {project_names}")
@@ -274,46 +312,45 @@ def buscar_datos_en_pdf():
         print(f"Propose Zoning: {propose_zoning}")
         print("Results saved to Excel file.")
 
-    # Habilitar el botón de búsqueda después de completar la tarea
-    btn_buscar.config(state='normal')
+    # Enable the search button after completing the task
+    btn_search.config(state='normal')
 
-# Función para seleccionar un archivo PDF
+# Function to select a PDF file
 
-
-def seleccionar_pdf():
+def select_pdf():
     global pdf_path, pdf_name, start_time
 
-    # Abrir el diálogo de selección de archivos
+    # Open the file selection dialog
     pdf_path = filedialog.askopenfilename(filetypes=[('PDF Files', '*.pdf')])
 
-    # Obtener el nombre del archivo PDF seleccionado
+    # Get the name of the selected PDF file
     pdf_name = os.path.splitext(os.path.basename(pdf_path))[0]
 
-    # Deshabilitar el botón de búsqueda mientras se ejecuta la tarea
-    btn_buscar.config(state='disabled')
+    # Disable the search button while the task is running
+    btn_search.config(state='disabled')
 
-    # Mostrar mensaje de inicio de búsqueda
-    lbl_mensaje.config(text='Buscando datos en el archivo PDF...')
+    # Display start search message
+    lbl_messagge.config(text='Searching data in the file...')
 
-    # Guardar el tiempo de inicio de ejecución
+    # Save the start time of execution
     start_time = time.time()
 
-    # Llamar a la función para buscar los datos en el archivo PDF
-    buscar_datos_en_pdf()
+    # Call the function to search for data in the PDF file
+    search_data()
 
 
-# Configuración de la interfaz gráfica
+# GUI configuration
 root = Tk()
-root.title('Buscador de datos en PDF')
+root.title('Data Tool')
 root.geometry('300x150')
 
-lbl_instrucciones = Label(root, text='Seleccione un archivo PDF:')
+lbl_instrucciones = Label(root, text='Select a PDF file:')
 lbl_instrucciones.pack(pady=10)
 
-btn_buscar = Button(root, text='Buscar', command=seleccionar_pdf)
-btn_buscar.pack()
+btn_search = Button(root, text='Search', command=select_pdf)
+btn_search.pack()
 
-lbl_mensaje = Label(root, text='')
-lbl_mensaje.pack(pady=10)
+lbl_messagge = Label(root, text='')
+lbl_messagge.pack(pady=10)
 
 root.mainloop()
